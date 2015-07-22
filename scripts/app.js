bbcf54e68904697ef15b105cf336cd2785612b40
Peter Müller <peter@baggers.io>
2015-07-22 21:49:07 +0200

    online & offline events added

--- scripts/app.js
+++ scripts/app.js
@@ -4,27 +4,36 @@
 
 	angular.module('app', [
 
-		'firebase'
+		'firebase',
+		'ngStorage'
 	])
 
-	.controller('ChatCtrl', ['$scope', '$firebaseArray', function ($scope, $firebaseArray) {
+	.controller('ChatCtrl', [
+		'$scope', '$firebaseArray', '$localStorage', 'connection',
+		function ($scope, $firebaseArray, $localStorage, connection) {
 
-		$scope.loggedIn;
-		$scope.nickName;
+		$scope.$storage = $localStorage;
 		$scope.message;
 		$scope.messages = $firebaseArray(new Firebase('https://vivid-fire-5234.firebaseio.com/messages'));
 		$scope.login = login;
+		$scope.logout = logout;
 		$scope.sendMessage = sendMessage;
+		$scope.connection = connection;
 
 		function login () {
 
-			$scope.loggedIn = true;
+			$scope.$storage.loggedIn = true;
+		}
+
+		function logout () {
+
+			$scope.$storage.loggedIn = false;
 		}
 
 		function sendMessage () {
 
 			$scope.messages.$add({
-				nickName: $scope.nickName,
+				nickName: $scope.$storage.nickName,
 				message: $scope.message,
 				dateTime: new Date().getTime()
 			});
@@ -32,6 +41,28 @@
 			$scope.message = '';
 		}
 
-	}]);
+	}])
+
+	.service('connection', function () {
+
+		this.online = navigator.onLine;
+
+
+
+		window.addEventListener('online',  function () {
+
+			console.log('online');
+
+			this.online = true;
+		}.bind(this), false);
+
+		window.addEventListener('offline', function () {
+
+			console.log('offline')
+
+			this.online = false;
+		}.bind(this), false);
+
+	});
 
 }());
